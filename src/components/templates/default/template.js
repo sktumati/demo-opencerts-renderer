69c907c997fd1ac03d6a0a0560923b0d701b2ed9
Raymond Yeh <yehjxraymond@gmail.com>
2019-07-01 16:17:45 +0800

    Added test and moved NestedComponent as a module

--- src/components/templates/default/template.js
+++ src/components/templates/default/template.js
@@ -1,42 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import "bootstrap/dist/css/bootstrap.css";
-
-const borderStyle = {
-  borderWidth: 1,
-  borderStyle: "solid",
-  borderColor: "#d5effc"
-};
-
-const KeyValueComponent = (key, value) => {
-  const [show, setShow] = useState(true);
-  const toggleShow = () => setShow(!show);
-  return (
-    <div key={key} style={borderStyle}>
-      <div
-        className="p-4"
-        style={{ backgroundColor: "#d5effc" }}
-        onClick={toggleShow}
-      >
-        {key}
-      </div>
-      <div className={`p-4 text-truncate ${show ? "" : "d-none"}`}>
-        {NestedComponent({ children: value })}
-      </div>
-    </div>
-  );
-};
-
-export const NestedComponent = ({ children }) => {
-  if (typeof children !== "object") {
-    return children.toString();
-  } else {
-    const grandchildren = Object.keys(children).map(key =>
-      KeyValueComponent(key, children[key])
-    );
-    return <div style={borderStyle}>{grandchildren}</div>;
-  }
-};
+import NestedComponent from "./NestedComponent";
 
 const Template = ({ document }) => (
   <div className="container">
