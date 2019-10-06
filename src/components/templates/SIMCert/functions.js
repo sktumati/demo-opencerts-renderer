8fc2b00a2d739c3f8bc7a806a337ad638c72b525
Suresh Kumar, tumati <tumati_kumar@yahoo.com>
2019-10-06 11:11:59 +0800

    Update functions.js

--- src/components/templates/SIMCert/functions.js
+++ src/components/templates/SIMCert/functions.js
@@ -2,7 +2,7 @@ import { tz } from "moment-timezone";
 import React from "react";
 import { format } from "date-fns";
 import { get } from "lodash";
-//import { toWordsOrdinal, toWords } from "number-to-words";
+
 
 export const TIMEZONE = "Asia/Singapore";
 
@@ -28,28 +28,7 @@ export const formatDDMONYYYY = dateString => {
   return tz(date, TIMEZONE).format("D MMMM YYYY");
 };
 
-export const formatDate = dateString => {
-  if (!dateString) return null;
-  const date = new Date(dateString);
 
-  // use moment-timezone, get the full year
-  const year = Number(tz(date, TIMEZONE).format("YYYY"));
-
-  // use number-to-word, convert to words
-  //let yearStr = toWords(year);
-
-  // change - begin with UpperCase, place commas with 'and'
-  yearStr = yearStr.replace(",", " and");
-  const yearReplacedStr = yearStr
-    .substring(0, 1)
-    .toUpperCase()
-    .concat(yearStr.substring(1));
-  return (
-    <span>
-      {tz(date, TIMEZONE).format("MMMM")} <br /> {yearReplacedStr}
-    </span>
-  );
-};
 
 export const formatNRIC = nricFin => {
   if (!nricFin) return null;
@@ -57,19 +36,7 @@ export const formatNRIC = nricFin => {
   return arrayNric.length === 3 ? arrayNric[2] : null;
 };
 
-export const formatDatePrefix = dateString => {
-  if (!dateString) return null;
-  const date = new Date(dateString);
 
-  // use moment-timezone, get the day
-  const day = Number(tz(date, TIMEZONE).format("D"));
-
-  // use number-to-words, toWordsOrdinal - output first, second etc.
-  //const strDay = toWordsOrdinal(day);
-
-  // eg first day of
-  return <span>{strDay} day of</span>;
-};
 
 export const formatCertName = meritFlag => (
   <p style={printMeritTextStyle}>{meritFlag === "Y" ? "WITH MERIT" : ""}</p>
